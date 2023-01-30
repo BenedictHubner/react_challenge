dc1f151b49812018388102003edfaa7202aab350
Ben Hubner <117365892+BenedictHubner@users.noreply.github.com>
2023-01-30 15:57:53 +0000

    Add AppointmentForm functionality

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -19,7 +19,23 @@ export const AppointmentForm = ({
     return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
   };
 
+  const handleTitleChange = e => {
+    setTitle(e.target.value);
+  }
+  const handleDateChange = e => {
+    setDate(e.target.value);
+  }
+  const handleTimeChange = e => {
+    setTime(e.target.value);
+  }
+
   return (
-    AppointmentForm
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={title} onChange={handleTitleChange} required />
+      <input type="date" value={date} onChange={handleDateChange} required min={getTodayString()} /> 
+      <input type="time" value={time} onChange={handleTimeChange} required/>
+      <ContactPicker contacts={contacts} />
+      <input type="submit" />
+    </form>
   );
 };
