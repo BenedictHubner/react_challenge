9399942fbbbad1402e6aca7e3112f9636ff35689
Ben Hubner <117365892+BenedictHubner@users.noreply.github.com>
2023-01-30 14:56:26 +0000

    Update ContactsForm

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -9,7 +9,23 @@ export const ContactForm = ({
   setEmail,
   handleSubmit
 }) => {
+  const handleNameChange = e => {
+    setName(e.target.value);
+  }
+  const handlePhoneChange = e => {
+    setPhone(e.target.value);
+  }
+  const handleEmailChange = e => {
+    setEmail(e.target.value);
+  }
+
+
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={name} onChange={handleNameChange} />
+      <input type="tel" value={phone} onChange={handlePhoneChange} pattern="[0-9]{5}" />
+      <input type="email" value={email} onChange={handleEmailChange} />
+      <input type="submit" />
+    </form>
   );
 };
